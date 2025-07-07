fa641fb6903f11174a2a8ed881293901d2a52a75
anyajegorova <anna.jerm@outlook.com>
2025-07-07 15:21:21 +0300

    footer and footer styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, EB_Garamond } from "next/font/google";
 import "./globals.css";
 import Nav from "./components/Nav";
+import Footer from "./components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} ${ebGaramond.variable}`}>
         <Nav />
         {children}
+        <Footer />
       </body>
     </html>
   );
